54c4ad83193612453354095d66d1e79a2d25f26c
Darwin Ramirez <rdarwinst@gmail.com>
2025-01-15 18:32:07 -0500

    Formulario Emergente

--- views/ayuda.php
+++ views/ayuda.php
@@ -3,9 +3,25 @@
     <span class="tooltip-text">¿Necesitas Ayuda?</span>
 </div>
 
-
-<form action="/" method="get" class="form-contactar">
+<form action="#" method="post" class="form-contactar">
     <span class="btn-cerrar">X</span>
-    <p class="text-white centrar-texto uppercase" style="font-weight: 700;">Ir al Formulario de Contacto</p>
-    <a href="/contacto" class="boton-blanco">Ir al formulario</a>
+    <div class="contenedor-campos">
+        <div class="campo">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre">
+        </div>
+        <div class="campo">
+            <label for="apellido">Apellido</label>
+            <input type="text" name="apellido" id="apellido">
+        </div>
+        <div class="campo">
+            <label for="email">Correo</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div class="campo">
+            <label for="telefono">Teléfono</label>
+            <input type="tel" name="telefono" id="telefono">
+        </div>
+    </div>
+    <input type="submit" value="Solicitar Ayuda" class="boton-blanco">
 </form>
